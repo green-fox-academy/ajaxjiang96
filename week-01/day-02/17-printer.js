29214eff071d32dd0348ca4debbfb374be602c21
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-15 14:33:31 +0800

    adding printer function

--- week-01/day-02/17-printer.js
+++ week-01/day-02/17-printer.js
@@ -2,3 +2,9 @@
 // - Create a function called `printer`
 //   which logs to the console the input parameters
 //   (can have multiple number of arguments)
+
+function printer(...params) {
+  params.forEach((param) => console.log(param));
+}
+
+printer('hello', 'my', 'name', 'is', '...');
