e9008dbc5fa0d689d400e1d00caa520456c5d3ca
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-21 14:50:50 +0800

    workshop: finished Reservation exercise

---
+++ week-02/day-01/reservation/Reservation.ts
@@ -0,0 +1,44 @@
+import IPrintable from "../printable/IPrintable";
+
+interface IReservationy {
+  getDowBooking(): string;
+  getCodeBooking(): string;
+}
+
+const DOW = [
+  "MON",
+  "TUE",
+  "WED",
+  "THU",
+  "FRI",
+  "SAT",
+  "SUN",
+];
+
+export default class Reservation implements IReservationy, IPrintable {
+  private dow: string;
+  private code: string;
+
+  constructor() {
+    this.dow = this.getDowBooking();
+    this.code = this.getCodeBooking();
+  }
+  public getDowBooking(): string {
+    const randDow = Math.floor(Math.random() * DOW.length);
+    return DOW[randDow];
+  }
+  public getCodeBooking(): string {
+    return Math.random().toString(36).substring(2, 10).toUpperCase();
+  }
+
+  public printAllFields(): void {
+    console.log(`Booking# ${this.code} for ${this.dow}`);
+  }
+}
+
+const reservations = new Array<Reservation>();
+for (let i = 0; i < 10; i++) {
+  reservations.push(new Reservation());
+}
+
+reservations.forEach((reservation) => reservation.printAllFields());
