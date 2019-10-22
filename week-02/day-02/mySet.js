aa22ae5a1bd81c65f34b5f8a4214e4d82b0162e9
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-22 16:26:44 +0800

    workshop: added includes function to mySet

--- week-02/day-02/mySet.js
+++ week-02/day-02/mySet.js
@@ -10,5 +10,7 @@ MySet.prototype.constructor = MySet;
 MySet.prototype.add = function (value) {
   if (Array.prototype.indexOf.call(this, value) === -1) Array.prototype.push.call(this, value);
 };
-
+MySet.prototype.includes = function (value) {
+  return this.indexOf(value) !== -1;
+};
 module.exports = MySet;
