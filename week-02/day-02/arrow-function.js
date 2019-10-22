d05efc6716f592d25e745849441f6189c2c08ecb
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-22 11:18:24 +0800

    workshop: finished arrow function exercise

---
+++ week-02/day-02/arrow-function.js
@@ -0,0 +1,24 @@
+function multiPurposeFunction(action) {
+  if (action) {
+    console.log(action(3));
+  }
+}
+
+function exampleNonAnonymFunction(param) {
+  return param * param;
+}
+
+function frameFunction() {
+  // this function call shows an example,
+  // but it is not using anonym functions
+  multiPurposeFunction(exampleNonAnonymFunction);
+  // write your code here\
+  // eslint-disable-next-line prefer-arrow-callback,func-names
+  multiPurposeFunction(function (param) {
+    return exampleNonAnonymFunction(param);
+  });
+
+  multiPurposeFunction((param) => exampleNonAnonymFunction(param));
+}
+
+frameFunction();
