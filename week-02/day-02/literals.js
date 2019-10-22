4a867f4ea50d3407248ee68f18bb704b164f12cf
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-22 12:01:59 +0800

    workshop: finished literals exercise

---
+++ week-02/day-02/literals.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* eslint-disable object-shorthand */
+function carWrapper(model, color, year, doors, specPropName, specPropValue) {
+  // the function should create and return an object with assigning the model,
+  // color year and doors to properties with the same name as the variable and
+  // it should assign to the specPropName named property the specPropValue
+  return {
+    model,
+    color,
+    year,
+    doors,
+    [specPropName]: specPropValue,
+  };
+}
+
+function carWrapperClassic(model, color, year, doors, specPropName, specPropValue) {
+  // the function should create and return an object with assigning the model,
+  // color year and doors to properties with the same name as the variable and
+  // it should assign to the specPropName named property the specPropValue
+  return {
+    model: model,
+    color: color,
+    year: year,
+    doors: doors,
+    [specPropName]: specPropValue,
+  };
+}
+
+console.log(JSON.stringify(carWrapper('Benz', 'black', 1886, 0, 'historical', true)));
+console.log(JSON.stringify(carWrapperClassic('Benz', 'black', 1886, 0, 'historical', true)));
+// expected output: { model: 'Benz', color: 'black', year: 1886, doors: 0, historical: true}
