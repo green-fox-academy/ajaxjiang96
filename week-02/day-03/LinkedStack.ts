281384e3f63b873b47187654d680bb6fe975a2bb
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-23 14:18:26 +0800

    workshop: implemented stack using my node (my linked list)

---
+++ week-02/day-03/LinkedStack.ts
@@ -0,0 +1,26 @@
+import { INode, IStack } from "./Interfaces";
+import MyLinkedList from "./LinkedList";
+// import MyNode from "./Node";
+
+export default class MyLinkedStack<T> extends MyLinkedList<T> implements IStack<T> {
+  public head: INode<T>;
+  public tail: INode<T>;
+  constructor() {
+    super();
+    // this.head = new MyNode<T>(null);
+    // this.tail = this.head;
+  }
+
+  public empty(): boolean {
+    return this.head.next === undefined || this.head.next === null;
+  }
+  public peek(): T {
+    return this.tail.value ? this.tail.value : null;
+  }
+  public push(value: T): void {
+    this.add(value);
+  }
+  public pop(): T {
+    return this.remove(this.size() - 1);
+  }
+}
