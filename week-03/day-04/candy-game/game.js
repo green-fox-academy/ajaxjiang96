a74149cf61385cd8781e549ee6422ba86a7a400d
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-10-31 12:52:48 +0800

    workshop: finished w3d4 candy game

--- week-03/day-04/candy-game/game.js
+++ week-03/day-04/candy-game/game.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 // - Gather 10.000 candies!
 // - Clicking the ‘Create candies’ button gives you 1 candy.
 // - You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
@@ -5,3 +6,50 @@
 //   - Use the 🍭 emoji to display the lollipops you have
 // - Display the candy producton rate in the `Candies / Second` row
 // - If you press the "make candy rain" button, the candy generation should speed up 10x
+
+let candies = 0;
+let lollipops = 3;
+let speed = 1.0;
+
+function createCandies() {
+  candies += 1;
+  document.querySelector('.candies').textContent = candies;
+}
+function renderLollipop(n) {
+  return [...Array(n)].map(() => '🍭').join('');
+}
+function render() {
+  document.querySelector('.lollypops').textContent = renderLollipop(lollipops);
+  document.querySelector('.candies').textContent = candies;
+  document.querySelector('.speed').textContent = Math.floor(lollipops / 10) * 1 * speed;
+}
+
+function buyLollipop() {
+  if (candies >= 100) {
+    lollipops += 1;
+    candies -= 100;
+  }
+  render();
+}
+
+function makeItRain() {
+  speed *= 10;
+  document.querySelector('.speed').textContent = Math.floor(lollipops / 10) * 1 * speed;
+}
+
+function main() {
+  document.querySelector('.create-candies').onclick = createCandies;
+  document.querySelector('.buy-lollypops').onclick = buyLollipop;
+  document.querySelector('.candy-machine').onclick = makeItRain;
+  const timer = setInterval(() => {
+    candies += Math.floor(lollipops / 10) * speed;
+    // console.log(candies)
+    render();
+    if (candies >= 10000) {
+      alert('You won!');
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+main();
