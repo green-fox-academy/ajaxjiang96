0ddd07561f7b1b0ff09751cabdf083e08f421e60
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-01 17:13:46 +0800

    style: w3d5 location added missing semicolon

--- week-03/day-05/location/script.js
+++ week-03/day-05/location/script.js
@@ -6,7 +6,7 @@ const ipApiKey = 'b381dfc2464e0e230fcf2b51a7a3e086';
 
 window.addEventListener('load', () => {
   const resultDiv = document.querySelector('.result');
-  resultDiv.innerHTML = `<iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=${mapApiKey}&q=Space+Needle,Seattle+WA" allowfullscreen />`
+  resultDiv.innerHTML = `<iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=${mapApiKey}&q=Space+Needle,Seattle+WA" allowfullscreen />`;
   const keyWordInput = document.querySelector('input#name');
   const form = document.querySelector('#search');
 
