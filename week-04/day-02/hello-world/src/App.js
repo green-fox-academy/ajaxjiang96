de2a088f96cceaa30cd2a3f3dd44ef736760abad
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-05 13:24:55 +0800

    workshop: finished w4d2 helloworld

---
+++ week-04/day-02/hello-world/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={() => <p>Hello World!</p>} />
+        <Route
+          path="/error/:errorcode"
+          render={({ match }) => (
+            <p>
+              Error:
+              {' '}
+              {match.params.errorcode}
+            </p>
+          )}
+        />
+        <Route
+          path="/error"
+          render={() => (
+            <p>
+              Something bad happened.
+            </p>
+          )}
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
