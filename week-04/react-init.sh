0a8474974d0bf9bdff6044d2754177efeb74c612
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-05 11:41:46 +0800

    workshop: adding a shorthand to init react project

---
+++ week-04/react-init.sh
@@ -0,0 +1,39 @@
+!/bin/sh
+echo "[React Init] Creating react project [$1] with create-react-app";
+npx create-react-app $1
+echo "[React Init] Installing prop-types";
+cd $1
+yarn add prop-types
+echo "[React Init] Setting up ESLint";
+yarn add -D eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
+echo '{
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": "off"
+    }
+}' > .eslintrc.json
+
+echo "[React Init] Finished setup";
+echo "[React Init] pwd: ";
+pwd;
