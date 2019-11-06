56578c2254459fbe6a6fc0eac7e409bae5e38237
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-06 13:02:27 +0800

    update: supressing eslint indent for w4d3 news

--- week-04/day-03/basic-news/src/ArticleListItem.js
+++ week-04/day-03/basic-news/src/ArticleListItem.js
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 const ArticleListItem = ({
   title, author, description, imgUrl,
 }) => (
+    // eslint-disable-next-line react/jsx-indent
     <section>
       <img src={imgUrl} alt={title} />
       <h1>{title}</h1>
       <h2>{author}</h2>
       <p>{description}</p>
     </section>
+    // eslint-disable-next-line indent
   );
 
 export default ArticleListItem;
