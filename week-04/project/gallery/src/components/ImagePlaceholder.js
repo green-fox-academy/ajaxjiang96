564f433085c2cc8c28f5e9ccffe19514988bf221
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-11 16:04:56 +0800

    update: linking gallery project as a submodule:

--- week-04/project/gallery/src/components/ImagePlaceholder.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ImagePlaceholder = () => <figure className="imgViewer loading"><div className="imgContainer"><div className="loader" /></div></figure>;
-
-export default ImagePlaceholder;
