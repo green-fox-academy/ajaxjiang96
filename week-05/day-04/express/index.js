ffeb6a308ab3d039d5821e46a415fbd11a4ba6c3
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-11-14 12:18:10 +0800

    workshop: w5d4 first server

---
+++ week-05/day-04/express/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname,
+  });
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
